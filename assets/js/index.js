df3ff804d298138622bdbcb65e97078cd636beae
Siria Franco <sirifranco00@gmail.com>
2023-04-10 13:14:16 -0700

    Boton para cliente

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,19 @@
 console.log("Sesion web1");
+
+function changeGreeting(){
+    // obtener nombre cliente
+    const clientName = getNameOfClient ();
+    // obtener referencia label h1
+    const greetingReference = getReferenceOfIdGreeting();
+    greetingReference.innerHTML = "Hola " + clientName;
+    // cambiar saludo
+}
+
+function getNameOfClient(){
+    const clientName = prompt("Escribe tu nombre");
+    return clientName;
+}
+
+function getReferenceOfIdGreeting(){
+    return document.getElementById("Greeting");
+}
